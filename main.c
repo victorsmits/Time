9dc30da562a26689f973f32adbc2bccbf19ce196
victorsmits <victor.smits@student.vinci.be>
2018-12-05 08:47:25 +0100

    first commit time function

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <hdf5.h>
+#include <unistd.h>
+
+struct time {
+    unsigned char hours ;
+    unsigned char minutes ;
+    unsigned char seconds ;
+};
+void increment_hour(struct time *time);
+
+
+int main( )
+{
+    struct time now={0,0,55};
+    while (1){
+        increment_hour(&now);
+        printf("%d:%d:%d\n",now.hours,now.minutes,now.seconds);
+        sleep(1);
+    }
+}
+
+void increment_hour(struct time *time){
+    time->seconds +=1;
+    if (time->seconds==60){
+        time->minutes +=1;
+        time->seconds=0;
+    }
+    if (time->minutes==60){
+        time->hours+=1;
+        time->minutes =0;
+    }
+}
