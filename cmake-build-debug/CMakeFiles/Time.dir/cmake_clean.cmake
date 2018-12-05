6f93b05cb882d58d75ac8edefb688960f52e7109
victorsmits <victor.smits@student.vinci.be>
2018-12-05 08:48:38 +0100

    fisrt commit

---
+++ cmake-build-debug/CMakeFiles/Time.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Time.dir/main.c.o"
+  "Time.pdb"
+  "Time"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Time.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
